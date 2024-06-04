e358b8c46ce4950815692fe35ad4a05718dc9341
Timothy Jason <timothycaviezel@gmail.com>
2024-06-04 21:02:39 +0700

    fixing

--- client/src/pages/RegisterPage.jsx
+++ client/src/pages/RegisterPage.jsx
@@ -1,10 +1,11 @@
 import { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 export default function RegisterPage() {
     const [username, setUsername] = useState('')
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
+    const navigate = useNavigate()
     async function handleRegisterUser(ev) {
         ev.preventDefault()
         const response = await fetch('http://localhost/damkar/api/register_user.php', {
@@ -20,24 +21,12 @@ export default function RegisterPage() {
         });
 
         const res = await response.json()
-        console.log(res);
-    }
-    async function handleRegisterAdmin(ev) {
-        ev.preventDefault()
-        const response = await fetch('http://localhost/damkar/api/register_admin.php', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({
-                username,
-                email,
-                password,
-            }),
-        });
-
-        const res = await response.json()
-        console.log(res);
+        if (res.message == 'Registrasi akun berhasil') {
+            alert(res.message)
+            navigate('/login')
+        } else {
+            alert(res.message)
+        }
     }
 
     return (
@@ -49,10 +38,6 @@ export default function RegisterPage() {
                     <input type="email" placeholder="your@email.com" value={email} onChange={ev => setEmail(ev.target.value)} />
                     <input type="password" placeholder="password" value={password} onChange={ev => setPassword(ev.target.value)} />
                     <button className="primary" onClick={handleRegisterUser}>Register as User</button>
-                    <div className="text-center py-2 text-gray-500">
-                        or
-                    </div>
-                    <button className="primary" onClick={handleRegisterAdmin}>Register as Admin</button>
                     <div className="text-center py-2 text-gray-500">
                         Already a member?
                         <Link className="underline text-black" to={'/login'}> Login now</Link>
